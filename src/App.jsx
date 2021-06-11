b2a99fbafbac01ce532dd7e5d43160aa1ebcb408
Viktor Panichkin <panichkinvictor@gmail.com>
2021-06-11 11:16:59 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './img/tier-logo.svg';
+import './css/App.css';
+import { ShortenUrlForm } from './components';
+
+function App() {
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Change mobility for good</p>
+            <ShortenUrlForm />
+        </div>
+    );
+}
+
+export default App;
